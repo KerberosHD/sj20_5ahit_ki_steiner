4d8ed05e4873973a725362876ba73a96edf0d42d
Tobias <t.steiner@htlkrems.at>
2021-05-27 18:54:00 +0200

    UE21_FaceRecegnition

---
+++ Uebung/UE21/UE21.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 21.1
+
+# In[3]:
+
+
+from PIL import Image
+import face_recognition
+
+# Load the jpg file into a numpy array
+image = face_recognition.load_image_file("biden.jpg")
+
+# Find all the faces in the image using the default HOG-based model.
+# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
+# See also: find_faces_in_picture_cnn.py
+face_locations = face_recognition.face_locations(image)
+
+print("I found {} face(s) in this photograph.".format(len(face_locations)))
+
+for face_location in face_locations:
+
+    # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+    # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
+
+# 21.2
+
+# In[ ]:
+
+
+from PIL import Image
+import face_recognition
+
+# Load the jpg file into a numpy array
+image = face_recognition.load_image_file("group.jpg")
+
+# Find all the faces in the image using the default HOG-based model.
+# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
+# See also: find_faces_in_picture_cnn.py
+face_locations = face_recognition.face_locations(image)
+
+print("I found {} face(s) in this photograph.".format(len(face_locations)))
+
+for face_location in face_locations:
+
+    # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+    # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
+
+# 21.3
+
+# In[5]:
+
+
+
+picture_of_biden = face_recognition.load_image_file("biden.jpg")
+my_face_encoding = face_recognition.face_encodings(picture_of_biden)[0]
+
+# my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
+
+unknown_picture = face_recognition.load_image_file("biden2.jpg")
+unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+
+# Now we can see the two face encodings are of the same person with `compare_faces`!
+
+results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
+
+if results[0] == True:
+    print("It's a picture of Biden!")
+else:
+    print("It's not a picture of Biden!")
+
+
+# 21.3
+
+# In[ ]:
+
+
+from PIL import Image, ImageDraw
+import face_recognition
+
+# Load the jpg file into a numpy array
+image = face_recognition.load_image_file("biden.jpg")
+
+# Find all facial features in all the faces in the image
+face_landmarks_list = face_recognition.face_landmarks(image)
+
+pil_image = Image.fromarray(image)
+for face_landmarks in face_landmarks_list:
+    d = ImageDraw.Draw(pil_image, 'RGBA')
+
+    # Make the eyebrows into a nightmare
+    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
+    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
+    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
+    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
+
+    # Gloss the lips
+    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
+    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+
+    # Sparkle the eyes
+    d.polygon(face_landmarks['left_eye'], fill=(255, 255, 255, 30))
+    d.polygon(face_landmarks['right_eye'], fill=(255, 255, 255, 30))
+
+    # Apply some eyeliner
+    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
+
+    pil_image.show()
+
